670531b005b17495bd70e3c5e96f598aedd5c420
NanaKwame <easj.asante@gmail.com>
2014-04-11 04:41:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TAShare::Application.config.secret_key_base = '0997cea9788fef2863c4ff1145ddcf3051d0dfb7f244d68f62ffca4db325f74f14711aeae61397f84f858fe712eccc1f5ab390bb0d12389c57594a8ffc659848'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TAShare::Application.config.session_store :cookie_store, key: '_TAShare_session'
