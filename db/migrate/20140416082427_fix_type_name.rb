86073c5ad2285463ad21684bfe4243deafef9d01
NanaKwame <easj.asante@gmail.com>
2014-04-25 13:45:04 -0400

    stuff

--- db/migrate/20140416082427_fix_type_name.rb
+++
@@ -1,5 +0,0 @@
-class FixTypeName < ActiveRecord::Migration
-  def change
-  	rename_column :likes, :type, :res_type
-  end
-end
--- db/migrate/20140416082857_fix_type_name_bookmark.rb
+++
@@ -1,5 +0,0 @@
-class FixTypeNameBookmark < ActiveRecord::Migration
-  def change
-  	rename_column :bookmarks, :type, :res_type
-  end
-end
