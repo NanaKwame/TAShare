6b9bf228ce3ddc9b58f7eaad2e3bdd18e4f045ea
Calvin Li <calvinli@mit.edu>
2014-05-06 22:44:10 -0400

    Changed div coloring, added overflow, better page responsiveness, title legend

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,49 +2,50 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
+//javascript for homepage
+var hpBuild = function() {
+	var hpContent = $("#hp-content")
+	var hpNavbar = $("#navbar");
+	var hpColumn = $(".hp-column");
+	var hpColumnTitle = $(".hp-column-title");
+	var hpColumnList = $(".hp-column-list");
+	var hpClassInfo = $(".hp-classInfo");
+	var hpClassTitle = $(".hp-classTitle");
 
+	var marginSize = 10;
+	var paddingSize = 10;
+	var navbarHeight = hpNavbar.height() + marginSize;
 
-	//javascript for homepage
-	var hpBuild = function() {
-		var hpContent = $("#hp-content")
-		var hpNavbar = $("#navbar");
-		var hpColumn = $(".hp-column");
-		var hpClassInfo = $(".hp-classInfo");
-		var hpClassTitle = $(".hp-classTitle");
-
-		var marginSize = 10;
-		var paddingSize = 10;
-		var navbarHeight = hpNavbar.height() + marginSize;
-
-		
+	//responsive page JS
+	var resizeFunc = function() {
+		//Fix height of page
 		var windowHeight = $(window).height() - 2*marginSize;
 		hpContent.css("margin-top", navbarHeight)
 			.height(windowHeight - navbarHeight - marginSize);
-
+		
+		//Get Width
 		var windowWidth = $(window).width() - 2*marginSize;
 
-		//column width
-		hpColumn.width(Math.floor((windowWidth - 2*paddingSize)/3.0));
-		hpColumn.height(Math.floor(hpContent.height() - 2*paddingSize));
+		//Choose between inline-block or stacked blocks
+		if (windowWidth < 720) {
+			hpColumn.width(windowWidth - 4*paddingSize);
+			hpColumn.css("height", "auto");
+		} else {
+			hpColumn.width(Math.floor((windowWidth - 2*paddingSize)/3.0) - 2*paddingSize);
+			hpColumn.height(Math.floor(hpContent.height() - 2*paddingSize));
+
+			hpColumnList.height(hpColumn.height() - hpColumnTitle.height() - 20);
+		}
 
 		//Set Vertical Align for class titles
 		hpClassTitle.css("margin-top", (hpClassInfo.height() - hpClassTitle.height())/2 + "px");
-
-		//responsive page JS
-		var windowVar = $(window);
-		$(window).resize(function() {
-			var windowHeight = windowVar.height() - 2*marginSize;
-			hpContent.css("margin-top", navbarHeight)
-				.height(windowHeight - navbarHeight - marginSize);
-			
-			var windowWidth = windowVar.width() - 2*marginSize;
-			hpColumn.width(Math.floor((windowWidth - 2*paddingSize)/3.0));
-			hpColumn.height(Math.floor(hpContent.height() - 2*paddingSize));
-
-			//Set Vertical Align for class titles
-			hpClassTitle.css("margin-top", (hpClassInfo.height() - hpClassTitle.height())/2 + "px");
-			
-		});
 	}
+
+	//Event listener for window resizing
+	$(window).resize(resizeFunc);
+
+	//Resize when loading page
+	resizeFunc();
+}
 
 
