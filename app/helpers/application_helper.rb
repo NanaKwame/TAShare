21e9fa659d6281db30b2b6d190d91d7d7fcaf316
NanaKwame <easj.asante@gmail.com>
2014-05-14 00:43:47 -0400

    fixed class list bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def classjson
     @class_list = []
     ClassTa.all.each do |classTT|
-      obj = {:name => classTT.name.to_s, :number => classTT.number.to_s, :id => classTT.id, :link  => url_for(:controller => "class_ta", :action => "show", :id => classTT.id).to_s}
+      obj = {:name => classTT.name.to_s, :number => classTT.number.to_s, :id => classTT.id, :link  => url_for(:controller => "/class_ta", :action => "show", :id => classTT.id).to_s}
       @class_list.push(obj)
     end
     @class_list
